7e3a71c29b4856c7fe702f94c8b1d30971bce3bc
Ercong Nie <66780877+ercong21@users.noreply.github.com>
2023-04-18 19:30:31 +0200

    add fewshot scripts

---
+++ xtreme/scripts/fewshot_xnli.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+REPO=$PWD
+GPU=${1:-0,1,2,3,4,5,6,7}
+MODEL=${2:-bert-base-multilingual-cased}
+DATA_DIR=${3:-"$REPO/download/"}
+OUT_DIR=${4:-"$REPO/outputs/"}
+
+export CUDA_VISIBLE_DEVICES=$GPU
+
+TASK='xnli'
+LR=2e-5
+EPOCH=5
+MAXL=128
+NUM_SAMPLE=8
+LANGS="ar,bg,de,el,es,fr,hi,ru,sw,th,tr,ur,vi,zh"
+LC=""
+if [ $MODEL == "bert-base-multilingual-cased" ]; then
+  MODEL_TYPE="bert"
+elif [ $MODEL == "xlm-mlm-100-1280" ] || [ $MODEL == "xlm-mlm-tlm-xnli15-1024" ]; then
+  MODEL_TYPE="xlm"
+  LC=" --do_lower_case"
+elif [ $MODEL == "xlm-roberta-large" ] || [ $MODEL == "xlm-roberta-base" ]; then
+  MODEL_TYPE="xlmr"
+fi
+
+if [ $MODEL == "xlm-mlm-100-1280" ] || [ $MODEL == "xlm-roberta-large" ]; then
+  BATCH_SIZE=2
+  GRAD_ACC=16
+  LR=3e-5
+else
+  BATCH_SIZE=1
+  GRAD_ACC=1
+  LR=2e-5
+fi
+
+# SAVE_DIR="$OUT_DIR/$TASK/${MODEL}-LR${LR}-epoch${EPOCH}-MaxLen${MAXL}/"
+SAVE_DIR="${OUT_DIR}/${TASK}/test_results_baseline/"
+# mkdir -p $SAVE_DIR
+
+python $PWD/run_baseline/run_classify.py \
+  --model_type $MODEL_TYPE \
+  --model_name_or_path $MODEL \
+  --train_language en \
+  --task_name $TASK \
+  --do_train \
+  --do_predict \
+  --data_dir $DATA_DIR/${TASK} \
+  --gradient_accumulation_steps $GRAD_ACC \
+  --per_gpu_train_batch_size $BATCH_SIZE \
+  --learning_rate $LR \
+  --num_train_epochs $EPOCH \
+  --max_seq_length $MAXL \
+  --output_dir $SAVE_DIR/ \
+  --save_steps 1000 \
+  --eval_all_checkpoints \
+  --log_file 'train' \
+  --predict_languages $LANGS \
+  --save_only_best_checkpoint \
+  --overwrite_output_dir \
+  --eval_test_set $LC \
+  --num_sample $NUM_SAMPLE
