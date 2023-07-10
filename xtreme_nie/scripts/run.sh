08783bdde3d799c4b7a4e000188009a660623ac9
boleima <107153303+boleima@users.noreply.github.com>
2023-07-10 16:49:04 +0200

    Delete xtreme_nie directory

--- xtreme_nie/scripts/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-scripts/./train_xnli.sh
-scripts/./train_prompt_xnli.sh
-scripts/./train_amazon.sh
-scripts/./train_prompt_amazon.sh
-scripts/./train_pawsx.sh
-scripts/./train_prompt_pawsx.sh
